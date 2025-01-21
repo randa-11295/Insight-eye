f08701cf6b80b2cb9946c608564c9f0b8389bf78
randa.ibrahim <randa.ibrahim@cartologic.com>
2025-01-21 20:37:16 +0200

    update content nav

--- src/utils/StaticVariables.js
+++ src/utils/StaticVariables.js
@@ -1,18 +1,41 @@
 // nav icons
-import ExploreOutlinedIcon from "@mui/icons-material/ExploreOutlined";
-import PublicOutlinedIcon from '@mui/icons-material/PublicOutlined';
-
+import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
+import LiveTvOutlinedIcon from '@mui/icons-material/LiveTvOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import CorporateFareOutlinedIcon from '@mui/icons-material/CorporateFareOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
 
 export const navbarContentArr = [
  {
-    text: "home",
+    text: "Dashboard",
     url: "/",
-    icon: <ExploreOutlinedIcon sx={{ marginLeft: " auto" }} />,
+    icon: <DashboardOutlinedIcon sx={{ marginLeft: " auto" }} />,
   },
   {
-    text: "test",
-    url: "/test",
-    icon: <PublicOutlinedIcon sx={{ marginLeft: " auto" }} />,
+    text: "Stream",
+    url: "/stream",
+    icon: <LiveTvOutlinedIcon sx={{ marginLeft: " auto" }} />,
+  },
+  {
+    text: "Search",
+    url: "/search",
+    icon: <SearchOutlinedIcon sx={{ marginLeft: " auto" }} />,
+  },
+  {
+    text: "Prediction",
+    url: "/production",
+    icon: <CorporateFareOutlinedIcon sx={{ marginLeft: " auto" }} />,
+  },
+  {
+    text: "Setting",
+    url: "/setting",
+    icon: <SettingsOutlinedIcon sx={{ marginLeft: " auto" }} />,
+  },
+  {
+    text: "Logout",
+    url: false,
+    icon: <LogoutOutlinedIcon sx={{ marginLeft: " auto" }} />,
   },
  
 
