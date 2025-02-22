ac849fe0a62be8d7a4f503296be6e466713a6c99
randa.ibrahim <randa.ibrahim@cartologic.com>
2025-02-22 23:04:07 +0200

    add search route

---
+++ src/Pages/Search.js
@@ -0,0 +1,9 @@
+const Search = ()=>{
+     return (
+          <h2>
+               this is search
+          </h2>
+     )
+}
+
+export default Search
--- src/routes/index.js
+++ src/routes/index.js
@@ -8,6 +8,7 @@ import HomePage from "../Pages/Home";
 import Dashboard from "../Pages/Dashboard";
 import AddStream from "../Pages/Stream/AddStream";
 import UpdateStreams from "../Pages/Stream/UpdateStreams";
+import Search from "../Pages/Search"
 import Stream from "../Pages/Stream/Streams"
 import ShowStreams from "../Pages/Stream/ShowStreams";
 
@@ -22,7 +23,7 @@ export const AppRouter = () => {
           <Route path="/streams/add-stream" element={<AddStream />} />
           <Route path="/streams/show-streams" element={<ShowStreams />} />
           <Route path="/streams/update-streams" element={<UpdateStreams />} />
-          <Route path="/search" element={<h1>search</h1>} />
+          <Route path="/search" element={<Search />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Route>
       </Routes>
