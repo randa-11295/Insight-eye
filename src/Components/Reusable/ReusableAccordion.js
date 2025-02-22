2fe57f9963af9d285ba60e3ed4ab45cc4f8fd25d
randa.ibrahim <randa.ibrahim@cartologic.com>
2025-02-22 23:35:37 +0200

    add  Accordion

---
+++ src/Components/Reusable/ReusableAccordion.js
@@ -0,0 +1,26 @@
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+export default function ReusableAccordion() {
+  return (
+    <div>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1-content"
+          id="panel1-header"
+        >
+          <Typography component="span">Accordion 1</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        </AccordionDetails>
+      </Accordion>
+    
+    </div>
+  );
+}
--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -1,9 +1,12 @@
 import Holder from "../Components/HOC/Holder"
 import ReusableToggleBtns from "../Components/Reusable/ReusableToggleBtns"
+import ReusableAccordion from "../Components/Reusable/ReusableAccordion"
+
 const Search = () => {
      return (
           <Holder>
                <ReusableToggleBtns />
+               <ReusableAccordion />
           </Holder>
      )
 }
