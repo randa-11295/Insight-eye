798fb0cf97d4a2e4e35076c7aa70dc0b47a9af50
randa.ibrahim <randa.ibrahim@cartologic.com>
2025-02-24 21:45:18 +0200

    remove bg from holder

--- src/Components/HOC/Holder.js
+++ src/Components/HOC/Holder.js
@@ -1,17 +1,17 @@
-import { Paper, Typography, Stack } from "@mui/material"
+import {  Typography, Stack } from "@mui/material"
 
-const Holder = ({ children, title, action }) => (
+const Holder = ({ children, title =" ", action }) => (
+<>
+<Stack p={3} direction="row" justifyContent="space-between" alignItems="center" >
 
-    <Paper sx={{ padding: "1rem", background: "#121212" }} elevation={3} >
-        <Stack direction="row" justifyContent="space-between" alignItems="center" >
+    {title && <Typography pb={2} fontWeight={800} fontSize={23} textTransform="capitalize">
+        {title }
+    </Typography>}
+    {action}
+</Stack>
+{children}
+</>
 
-            {title && <Typography pb={2} fontWeight={800} fontSize={23} textTransform="capitalize">
-                {title}
-            </Typography>}
-            {action}
-        </Stack>
-        {children}
-    </Paper>
 )
 
 export default Holder
