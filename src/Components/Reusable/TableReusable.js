7986a6f46132ffc1c74d962aa328b140665df950
randa.ibrahim <randa.ibrahim@cartologic.com>
2025-02-24 21:04:33 +0200

    update header style in table

--- src/Components/Reusable/TableReusable.js
+++ src/Components/Reusable/TableReusable.js
@@ -6,15 +6,21 @@ const TableReusable = ({ data, columns, loading }) => {
   if (loading) return <TableLoader  columns={columns.length} />; // Show loader if loading
 
   return (
-    <TableContainer component={Paper} sx={{ mt: 3 }}>
+    <TableContainer component={Paper} sx={{ mt: 3 , border: "2px solid gray", }} >
       <Table>
-        <TableHead>
-          <TableRow>
-            {columns.map((col) => (
-              <TableCell align="center" key={col.field}>{col.headerName}</TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
+      <TableHead >
+  <TableRow>
+    {columns.map((col) => (
+      <TableCell
+        align="center"
+        key={col.field}
+        sx={headerStyle}
+      >
+        {col.headerName}
+      </TableCell>
+    ))}
+  </TableRow>
+</TableHead>
         <TableBody>
           {data.length > 0 ? (
             data.map((row, rowIndex) => (
@@ -22,7 +28,7 @@ const TableReusable = ({ data, columns, loading }) => {
                 {columns.map((col) => (
                   <TableCell key={col.field} align="center">
                     {col.field === "frame" ? (
-                      <img src={`data:image/jpeg;base64,${row[col.field]}`} alt="Frame" width="80" />
+                      <img src={`data:image/jpeg;base64,${row[col.field]}`} alt="Frame" width="50" />
                     ) : (
                       row[col.field] ||  row.metadata[col.field]
                     )}
@@ -44,3 +50,14 @@ const TableReusable = ({ data, columns, loading }) => {
 };
 
 export default TableReusable;
+
+const headerStyle={
+  fontWeight: "bold",
+  textTransform: "uppercase",
+  fontSize: "16px",
+  letterSpacing: "0.8px",
+  padding: "12px",
+  color: "primary.main",
+  borderBottom: "2px solid gray",
+  paddingY : 3
+}
