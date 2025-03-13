5d1a4413be7fc0ec41e093603b8448ba7256e734
randa.ibrahim <randa.ibrahim@cartologic.com>
2025-03-13 06:29:59 +0200

    add System information
     in dashbourd

--- src/Components/HOC/Holder.js
+++ src/Components/HOC/Holder.js
@@ -1,10 +1,10 @@
 import {  Typography, Stack } from "@mui/material"
 
-const Holder = ({ children, title =" ", action }) => (
+const Holder = ({ children, title, action }) => (
 <>
-<Stack p={3} direction="row" justifyContent="space-between" alignItems="center" >
+<Stack py={2} direction="row" justifyContent="space-between" alignItems="center" >
 
-    {title && <Typography pb={2} fontWeight={800} fontSize={23} textTransform="capitalize">
+    {title && <Typography  fontWeight={800} fontSize={21} textTransform="capitalize">
         {title }
     </Typography>}
     {action}
--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,23 +1,49 @@
 import React from "react";
-import { Paper } from "@mui/material";
-import Grid from '@mui/material/Grid2';
-
+import { Paper, Stack, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import Holder from "../Components/HOC/Holder";
 const MyGridLayout = () => {
     return (
         <Grid container spacing={2}>
             {/* Top Left */}
             <Grid item xs={12} sm={6}>
-                <Paper sx={{ padding: 2, textAlign: "center" }}>Section 1</Paper>
+                <Holder title="test">
+                    <Paper sx={{ padding: 2, textAlign: "center" }}>Section 2</Paper>
+
+                </Holder>
             </Grid>
 
             {/* Top Right */}
             <Grid item xs={12} sm={6}>
-                <Paper sx={{ padding: 2, textAlign: "center" }}>Section 2</Paper>
+                <Holder title="test">
+                    <Paper sx={{ padding: 2, textAlign: "center" }}>Section 2</Paper>
+
+                </Holder>
             </Grid>
 
             {/* Full-Width Row */}
             <Grid item xs={12}>
-                <Paper sx={{ padding: 2, textAlign: "center" }}>Section 3</Paper>
+                <Holder title="System information">
+                    <Paper sx={{ padding: 2, textAlign: "center" }}>
+
+                        <Stack direction={{sm:"row"}} justifyContent="space-between">
+                            <Typography>
+                                Frame Rate :
+                                <Typography component="b" color="primary.main" sx={{ fontWeight: "900", margin: "10px" }}>
+                                    30
+                                </Typography>
+                            </Typography>
+                   
+                            <Typography>
+                                Confidence Level :
+                                <Typography component="b" color="primary.main" sx={{ fontWeight: "900", margin: "10px" }}>
+                                    0.60
+                                </Typography>
+                            </Typography>
+                        </Stack>
+                    </Paper>
+
+                </Holder>
             </Grid>
         </Grid>
     );
