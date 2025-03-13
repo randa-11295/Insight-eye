26ce989824c85fbdf14d30ea9053429c75dbfcd4
randa.ibrahim <randa.ibrahim@cartologic.com>
2025-03-13 05:13:25 +0200

    add gris in dashbourd

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,13 +1,25 @@
-import Holder from "../Components/HOC/Holder"
-const Dashboard = () => {
-    return (
-        <>
-            < Holder title="chart title">
-                {/* <CustomChart /> */}
-                test
-            </Holder>      
-        </>
-    )
-}
+import React from "react";
+import { Grid, Paper } from "@mui/material";
 
-export default Dashboard 
+const MyGridLayout = () => {
+  return (
+    <Grid container spacing={2}>
+      {/* Top Left */}
+      <Grid item xs={12} sm={6}>
+        <Paper sx={{ padding: 2, textAlign: "center" }}>Section 1</Paper>
+      </Grid>
+
+      {/* Top Right */}
+      <Grid item xs={12} sm={6}>
+        <Paper sx={{ padding: 2, textAlign: "center" }}>Section 2</Paper>
+      </Grid>
+
+      {/* Full-Width Row */}
+      <Grid item xs={12}>
+        <Paper sx={{ padding: 2, textAlign: "center" }}>Section 3</Paper>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default MyGridLayout;
