7e58a9eb4595282ec0a2baea5895db2d208c359b
randa.ibrahim <randa.ibrahim@cartologic.com>
2025-03-26 08:53:13 +0200

    add list instead of pragragh in info component

--- src/Components/Auth/Info.js
+++ src/Components/Auth/Info.js
@@ -1,71 +1,67 @@
-import logo from "../../Images/logo.png"
-import bg from "../../Images/infobg.jpg"
+import logo from "../../Images/logo.png";
+import bg from "../../Images/infobg.jpg";
 import { Box, Stack, Typography } from "@mui/material";
 
 const Info = () => {
-     return (
-          <Stack sx={boxStyle} >
+  return (
+    <Stack sx={boxStyle}>
+      <Box sx={logoStyle}>
+        <img src={logo} alt="Logo" />
+      </Box>
+      <Stack sx={contentStyle}>
+        <Typography variant="h1" sx={titleStyle}>
+          Welcome in <br /> INSIGHT EYE
+        </Typography>
 
-               <Box sx={logoStyle}   >
-                    <img src={logo} alt="Logo" />
-               </Box>
-               <Stack sx={contentStyle}>
-                    <Typography
-                         variant="h1"
-                         sx={titleStyle}
-                    >
-                         Welcome in <br /> INSIGHT EYE
-                    </Typography>
+        <Typography component="ul"  sx={desStyle}>
+          <Typography component="li" sx={desStyle }>frist one</Typography>
+          <Typography component="li" sx={desStyle} >second on</Typography>
+          <Typography component="li" sx={desStyle}> thered on</Typography>
+        </Typography>
+      </Stack>
+    </Stack>
+  );
+};
 
-                    <Typography
-                         variant="body1"
-                         sx={desStyle}
-                    >
-                         Lorem ipsum was conceived as filler text, formatted in a certain way to enable the presentation of graphic elements in documents, without the need for formal copy. Using Lorem Ipsum allows designers to put together layouts and the form of the content before the content has been created, giving the design and production process more freedom.
-
-                         It is widely believed that the history of Lorem Ipsum originates with Cicero in the 1st Century BC and his text De
-                    </Typography>
-               </Stack>
-          </Stack>
-     )
-}
-
-export default Info
+export default Info;
 
 const boxStyle = {
-     width: { md: "50%" },
-     minHeight: { xs: "50%", md: "100%" },
-     p: 4,
-     backgroundImage: `url(${bg})`,
-     backgroundSize: "cover",
-     backgroundPosition: "left",
-     border: 3,
-     borderRadius: 5,
-     borderColor: "primary.main"
-}
-
+  width: { md: "50%" },
+  minHeight: { xs: "50%", md: "100%" },
+  p: 4,
+  backgroundImage: `url(${bg})`,
+  backgroundSize: "cover",
+  backgroundPosition: "left",
+  border: 3,
+  borderRadius: 5,
+  borderColor: "primary.main",
+};
 
 const logoStyle = {
-     width: { xs: "40%", lg: "28%" }, // Responsive width
-     display: "flex",
-     justifyContent: "center",
+  width: { xs: "40%", lg: "28%" }, // Responsive width
+  display: "flex",
+  justifyContent: "center",
 
-     "& img": {
-          width: "100%",
-          height: "auto",
-          objectFit: "contain",
-     },
-}
-const contentStyle = { textAlign: { xs: "center", md: "left " }, p: 2, flexGrow: 1, justifyContent: "center" }
+  "& img": {
+    width: "100%",
+    height: "auto",
+    objectFit: "contain",
+  },
+};
+const contentStyle = {
+  textAlign: { xs: "center", md: "left " },
+  p: 2,
+  flexGrow: 1,
+  justifyContent: "center",
+};
 
 const titleStyle = {
-     fontSize: { xs: "24px", sm: "32px", md: "40px", lg: "4rem" }, // Responsive font sizes
-     fontWeight: "bold",
-
-}
+  fontSize: { xs: "24px", sm: "32px", md: "40px", lg: "4rem" }, // Responsive font sizes
+  fontWeight: "bold",
+};
 
 const desStyle = {
-     fontSize: { xs: "14px", sm: "16px", md: "18px" }, // Responsive font sizes
-     color: "text.secondary",
-     mt: 4,
-}
+  fontSize: "1.5rem", // Responsive font sizes
+  color: "text.secondary",
+m :1
+};
