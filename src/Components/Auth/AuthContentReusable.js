b11b925bdc60d4c9ac6e8c0ec9562226f4dca801
randa.ibrahim <randa.ibrahim@cartologic.com>
2025-04-14 21:25:58 +0200

    back toreusable component

---
+++ src/Components/Auth/AuthContentReusable.js
@@ -0,0 +1,92 @@
+import InputTextCustom from "../Inputs/InputTextCustom";
+import LoadBtn from "../Reusable/LoadBtn";
+import { Box, Link, Typography, Stack } from "@mui/material";
+
+const AuthContentReusable = ({
+  title,
+  des,
+  formik,
+  children,
+  contentRoute,
+  footerRoute,
+  btnText,
+  loading,
+}) => {
+  return (
+    <Stack justifyContent="space-around" gap={3} sx={{ height: "100%" }}>
+      <Box>
+        <Typography
+          variant="h4"
+          sx={{
+            fontSize: "3rem",
+            fontWeight: "bold",
+            color: "primary.main",
+          }}
+        >
+          {title}
+        </Typography>
+
+        <Typography
+          variant="body1"
+          sx={{
+            color: "text.secondary",
+            mt: 1,
+          }}
+        >
+          {des}
+        </Typography>
+      </Box>
+
+      <Box component="form" onSubmit={formik.handleSubmit}>
+        {Object.keys(formik.initialValues).map((fieldName) => (
+          <InputTextCustom
+            key={fieldName}
+            label={fieldName}
+            placeholder={`Enter your ${fieldName}`}
+            type={fieldName === "password" ? "password" : "text"}
+            formik={formik}
+            name={fieldName}
+            value={formik.values[fieldName]}
+            onChange={formik.handleChange}
+          />
+        ))}
+
+        <LoadBtn
+          submit
+          fullWidth
+          text={btnText}
+          handle={() => formik.handleSubmit()}
+          loading={loading}
+        />
+
+        {contentRoute && (
+          <Link
+            component="span"
+            underline="hover"
+            sx={{
+              cursor: "pointer",
+              padding: "20px",
+              display: "block",
+              textAlign: "center",
+            }}
+          >
+            {contentRoute.linkText}
+          </Link>
+        )}
+      </Box>
+
+      <Typography sx={{ textAlign: "center" }}>
+        {footerRoute.title}
+        <Link
+          component="span"
+          underline="hover"
+          sx={{ cursor: "pointer", padding: "10px" }}
+        >
+          {footerRoute.linkText}
+        </Link>
+      </Typography>
+    </Stack>
+  );
+};
+
+export default AuthContentReusable;
--- src/Pages/auth/LogIn.js
+++ src/Pages/auth/LogIn.js
@@ -1,17 +1,17 @@
-import InputTextCustom from "../../Components/Inputs/InputTextCustom";
-import LoadBtn from "../../Components/Reusable/LoadBtn";
 import { useFormik } from "formik";
-import { Box, Link, Typography, Stack } from "@mui/material";
 import { useState } from "react";
-import { snackAlertState, authState } from "../../Recoil/RecoilState";
+import { snackAlertState } from "../../Recoil/RecoilState";
 import { useSetRecoilState } from "recoil";
+import { authState } from "../../Recoil/RecoilState";
+import { useAxiosWithAuth } from "../../services/api";
+import AuthContentReusable from "../../Components/Auth/AuthContentReusable";
 import axios from "axios";
 import { baseURL } from "../../utils/StaticVariables";
-import { Link as RouterLink } from "react-router-dom"; // ✅ Add this import at the top
 const LogIn = () => {
   const [loading, setLoading] = useState(false);
-  const setAuthRecoil = useSetRecoilState(authState);
   const setSnackAlert = useSetRecoilState(snackAlertState);
+  const setAuthRecoil = useSetRecoilState(authState);
+
 
   const showError = () => {
     setSnackAlert({
@@ -32,7 +32,6 @@ const LogIn = () => {
       axios
         .post(baseURL + "login", values)
         .then((response) => {
-          console.log("done", response);
           localStorage.setItem("token", response.data.access_token);
           localStorage.setItem("refresh_token", response.data.refresh_token);
           localStorage.setItem("expire", response.data.expires_at);
@@ -42,8 +41,10 @@ const LogIn = () => {
             refreshToken: response.data.refresh_token,
             expire: response.data.expires_at,
           });
+          window.location.reload();
         })
         .catch((error) => {
+          console.log("his error", error);
           showError();
         })
         .finally(() => {
@@ -53,82 +54,20 @@ const LogIn = () => {
   });
 
   return (
-    <Stack justifyContent="space-around" gap={4} sx={{ height: "100%" }}>
-      <Box>
-        <Typography
-          variant="h4"
-          sx={{
-            fontSize: "3rem",
-            fontWeight: "bold",
-            color: "primary.main",
-          }}
-        >
-          LOGIN
-        </Typography>
-
-        <Typography
-          variant="body1"
-          sx={{
-            color: "text.secondary",
-            mt: 1,
-          }}
-        >
-          login to your account to access all features in INSIGHT EYE
-        </Typography>
-      </Box>
-
-      <Box component="form" onSubmit={formik.handleSubmit}>
-        <InputTextCustom
-          label="Username "
-          placeholder="Enter your user name"
-          formik={formik}
-          name="username"
-        />
-
-        <InputTextCustom
-          label="password"
-          placeholder="Enter your password"
-          type="password"
-          formik={formik}
-          name="password"
-          value={formik.values.username}
-          onChange={formik.handleChange}
-        />
-
-        <LoadBtn
-          submit
-          fullWidth
-          text={"LogIn"}
-          handle={() => formik.handleSubmit()}
-          loading={loading}
-        />
-
-        <Link
-          component="span"
-          underline="hover"
-          sx={{
-            cursor: "pointer",
-            padding: "20px",
-            display: "block",
-            textAlign: "center",
-          }}
-        >
-          Forget your password ?
-        </Link>
-      </Box>
-
-      <Typography sx={{ textAlign: "center" }}>
-        New user ?
-        <Link
-          component={RouterLink}
-          to="/contact"
-          underline="hover"
-          sx={{ cursor: "pointer", padding: "10px" }}
-        >
-          Contact us
-        </Link>
-      </Typography>
-    </Stack>
+    <AuthContentReusable
+      formik={formik}
+      title="Login"
+      btnText={"Send Massage"}
+      des="login to your account  to access all features in INSIGHT EYE"
+      contentRoute={{ linkText: " Forget your password ?", route: "/" }}
+      loading={loading}
+      footerRoute={{
+        title: "  New user ?",
+        linkText: " Contact us",
+        route: "/",
+      }}
+    />
+   
   );
 };
 
