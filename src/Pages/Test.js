f8f8d8f0b5c1adc7ad19944c61ed0ca502f17a80
randa.ibrahim <randa.ibrahim@cartologic.com>
2025-04-25 17:44:57 +0300

    add SubscriptionWarningCard

--- src/Pages/Test.js
+++ src/Pages/Test.js
@@ -1,10 +1,12 @@
 import PramStream from "../Components/Auth/Stream/PramStream";
+import SubscriptionWarningCard from "../Components/Cards/SubscriptionWarningCard";
 const Test = () => {
   return (
     <>
       <PramStream />
+      <SubscriptionWarningCard />
     </>
   );
 };
 
-export default PramStream
+export default Test;
