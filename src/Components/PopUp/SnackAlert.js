f2c1750d05c759a8f4ab5ffb27e867ef2e854810
randa.ibrahim <randa.ibrahim@cartologic.com>
2025-04-29 17:25:02 +0300

    fix alert issue

--- src/Components/PopUp/SnackAlert.js
+++ src/Components/PopUp/SnackAlert.js
@@ -1,13 +1,7 @@
 import { useEffect } from "react";
 import { useRecoilState } from "recoil";
 import Snackbar from "@mui/material/Snackbar";
-import MuiAlert from "@mui/material/Alert";
-import Slide from "@mui/material/Slide";
 import { snackAlertState } from "../../Recoil/RecoilState"; // Import Recoil state
-
-function Transition(props) {
-  return <Slide {...props} direction="left" />;
-}
 
 export default function SnackAlert() {
   const [snackState, setSnackState] = useRecoilState(snackAlertState);
@@ -28,21 +22,10 @@ export default function SnackAlert() {
   return (
     <Snackbar
       open={snackState.open}
-      anchorOrigin={{
-        vertical: "bottom",
-        horizontal: "right",
-      }}
+      autoHideDuration={5000}
       sx={{ width: { md: "300px" } }}
-      TransitionComponent={Transition}
-    >
-      <MuiAlert
-        severity={snackState.severity}
-        sx={{ width: "100%" }}
-        onClose={handleClose}
-        variant="filled"
-      >
-        {snackState.message || "Everything is okay"}
-      </MuiAlert>
-    </Snackbar>
+      onClose={handleClose}
+      message={snackState.message || "Everything is okay"}
+    />
   );
 }
